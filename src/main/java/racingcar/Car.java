e6e718ccdafb65ee4caa29284c4a9ba79034e952
jinny-l <jinny.l.dev@gmail.com>
2022-11-27 17:59:07 +0900

    feat(Car): 자동차 이름 입력 값의 유효성 검증 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,21 @@
 package racingcar;
 
 public class Car {
+
+    private static final int NAME_LENGTH = 5;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateNameLength(name);
         this.name = name;
     }
 
-    // 추가 기능 구현
+    private void validateNameLength(String name) {
+        if (name.length() > NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
