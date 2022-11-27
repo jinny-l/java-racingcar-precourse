835b3d2ec86a1ae98e07872ebb9febd4b5504f93
jinny-l <jinny.l.dev@gmail.com>
2022-11-27 22:08:10 +0900

    refactor(InputView): 유효성 검증하는 메서드를 클래스 내부에 구현
    - 목적:
    책임질 필요 없는 검증 메서드 삭제
    책임져야 할 검증 메서드 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,31 +4,65 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import racingcar.util.InputValidator;
+import racingcar.domain.Car;
 
 public class InputView {
 
-    private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(%s) 기준으로 구분%n)";
+    private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(%s) 기준으로 구분)%n";
     private static final String CAR_NAME_DELIMITER = ",";
     private static final String INPUT_ATTEMPTS = "시도할 회수는 몇회인가요?";
 
+    /**
+     * 자동차 이름을 입력할 때 사용하는 메서드
+     * @return 2개 이상 입력 시, 쉼표 구분 여부를 검증하고 입력 값을 반환함
+     */
     public static List<String> readCarName() {
         System.out.printf(INPUT_CAR_NAME, CAR_NAME_DELIMITER);
         String input = readInput();
-        InputValidator.validateSeparatedByComma(input);
-        return Stream.of(input.split(CAR_NAME_DELIMITER))
+        validateSeparatedByComma(input, CAR_NAME_DELIMITER);
+        return Stream.of(input.split("\\s*" + CAR_NAME_DELIMITER + "\\s*"))
                 .collect(Collectors.toList());
     }
+
+    public static void validateSeparatedByComma(String input, String delimiter) {
+        if (input.length() > Car.getNameLength() && !input.contains(delimiter)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * 사용자가 시도 회수를 입력할 때 사용하는 메서드
+     * @return 정수 여부를 검증하고 입력 값을 반환함
+     */
 
     public static int readAttempts() {
         System.out.println(INPUT_ATTEMPTS);
         String input = readInput();
-        int attempts = InputValidator.validateInteger(input);
-        InputValidator.validateNumberRange(attempts);
-        return attempts;
+        return validateInteger(input);
     }
 
-    private static String readInput() {
-        return Console.readLine().trim();
+    public static int validateInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException();
+        }
     }
+
+    /**
+     * 사용자의 입력을 받는 메서드
+     * @return 공백을 검증하고 입력 값을 반환함
+     */
+    private static String readInput() {
+        String input = Console.readLine().trim();
+        validateHasInput(input);
+        return input;
+    }
+
+    private static void validateHasInput(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
