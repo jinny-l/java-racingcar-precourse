b90848fe516f34e1aa3c56e59b67480bb8409957
jinny-l <jinny.l.dev@gmail.com>
2022-11-28 22:11:55 +0900

    feat: 테스트를 위헤 랜덤 숫자를 생성하는 인터페이스 구현

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -2,12 +2,12 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumberGenerator {
+public class RandomNumberGenerator implements NumberGenerator {
 
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
 
-    public static int getNumber() {
+    public int generate() {
         return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 
