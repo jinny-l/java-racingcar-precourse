c8c291123837e3cd5839754c1dbe82b26f82af4e
jinny-l <jinny.l.dev@gmail.com>
2022-11-28 20:43:14 +0900

    feat(GameController): 게임을 실행하는 Controller 기능 구현

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,68 @@
+package racingcar.controller;
+
+import racingcar.domain.Attempts;
+import racingcar.domain.CarNames;
+import racingcar.domain.Cars;
+import racingcar.service.RacingGame;
+import racingcar.util.RandomNumberGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameController {
+
+    private CarNames carNames;
+    private Attempts attempts;
+    private Cars cars;
+    private int numberOfAttempts;
+
+    public void run() {
+        initialize();
+        printResult();
+        printWinner();
+    }
+
+    /**
+     * 게임 시작 시 사용하는 메서드
+     */
+    private void initialize() {
+        readCarName();
+        readAttempts();
+        cars = new Cars(carNames.getNames());
+        numberOfAttempts = attempts.get();
+    }
+
+    private void readCarName() {
+        try {
+            carNames = new CarNames(InputView.readCarName());
+        } catch (IllegalArgumentException ie) {
+            readCarName();
+        }
+    }
+
+    private void readAttempts() {
+        try {
+            attempts = new Attempts(InputView.readAttempts());
+        } catch (IllegalArgumentException ie) {
+            readAttempts();
+        }
+    }
+
+    /**
+     * 실행 결과 출력 시 사용하는 메서드
+     */
+    private void printResult() {
+        for (int i = 0; i < numberOfAttempts; i++) {
+            cars.move(RandomNumberGenerator.getNumber());
+            OutputView.printResult(cars.getCarsInformation());
+        }
+    }
+
+    /**
+     * 우승자 출력 시 사용하는 메서드
+     */
+    private void printWinner() {
+        RacingGame racingGame = new RacingGame(cars);
+        OutputView.printWinner(racingGame.getWinnerNames());
+    }
+
+}
