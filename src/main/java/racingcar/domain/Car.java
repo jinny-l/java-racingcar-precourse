7a6d51203b83726c648280426d9fe19626e513f0
jinny-l <jinny.l.dev@gmail.com>
2022-11-28 19:41:11 +0900

    feat(Car): 자동차가 이동한 결과를 표현하는 기능 구현
    - 기타 수정 사항:
    getter 메서드 리턴타입을 String으로 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,16 +2,22 @@ package racingcar.domain;
 
 import racingcar.information.GameRule;
 
+/**
+ * 자동차 1대를 관리하는 객체
+ */
+
 public class Car {
 
-    private final CarName name;
+    private static final String POSITION_EXPRESSION = "-";
+
+    private final String name;
     private int position = 0;
 
-    public Car(CarName name) {
+    public Car(String name) {
         this.name = name;
     }
 
-    public CarName getName() {
+    public String getName() {
         return name;
     }
 
@@ -19,10 +25,23 @@ public class Car {
         return position;
     }
 
+    /**
+     * 자동차를 한 칸 이동할 때 사용하는 메서드
+     * @param number : 랜덤 숫자
+     */
     public void moveForward(int number) {
         if (number >= GameRule.MOVE_FORWARD.getNumber()) {
             position++;
         }
+    }
+
+    /**
+     * 자동차가 이동한 결과를 표현할 때 사용하는 메서드
+     * @return 이동한 만큼 "-"를 String으로 반환함
+     */
+    @Override
+    public String toString() {
+        return POSITION_EXPRESSION.repeat(position);
     }
 
 }
