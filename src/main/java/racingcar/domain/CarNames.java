40aa2bb92b5a1dbc80dda5d2b89057f432686c09
jinny-l <jinny.l.dev@gmail.com>
2022-11-28 19:44:25 +0900

    feat(CarNames): CarNames을 반환하는 getter 메서드 구현

--- src/main/java/racingcar/domain/CarNames.java
+++ src/main/java/racingcar/domain/CarNames.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 자동차 이름 여러개를 관리하는 객체
@@ -16,6 +17,12 @@ public class CarNames {
         for (String carName : carNames) {
             this.carNames.add(new CarName(carName));
         }
+    }
+
+    public List<String> getNames() {
+        return carNames.stream()
+                .map(CarName::getName)
+                .collect(Collectors.toList());
     }
 
     private void validateUniqueName(List<String> carNames) {
